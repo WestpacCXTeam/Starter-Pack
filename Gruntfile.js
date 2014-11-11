e414f513f82fbaf6cb839afb917b468e4ebc60ae
sean <seankerr@westpac.com.au>
2014-11-12 10:01:28 +1100

    Changes
    Correct spelling mistakes etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,16 +59,16 @@ module.exports = function(grunt) {
 			scaffold: {
 				options: {
 					create: [
-						'./_CORE/_HTML/_tamples',
+						'./_CORE/_HTML/_templates',
 						'./_CORE/_HTML/_views',
-						'./_CORE/_JS',
+						'./_CORE/_js',
 						'./_CORE/_less/base',
 						'./_CORE/_less/modules',
 						'./_CORE/_Mock',
 
 						'./BOM/_css/png',
 						'./BOM/_fonts',
-						'./BOM/_HTMLincludes',
+						'./BOM/_HTML/includes',
 						'./BOM/_img',
 						'./BOM/_js/libs',
 						'./BOM/_less',
@@ -76,7 +76,7 @@ module.exports = function(grunt) {
 
 						'./BSA/_css/png',
 						'./BSA/_fonts',
-						'./BSA/_HTMLincludes',
+						'./BSA/_HTML/includes',
 						'./BSA/_img',
 						'./BSA/_js/libs',
 						'./BSA/_less',
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
 
 						'./STG/_css/png',
 						'./STG/_fonts',
-						'./STG/_HTMLincludes',
+						'./STG/_HTML/includes',
 						'./STG/_img',
 						'./STG/_js/libs',
 						'./STG/_less',
@@ -92,7 +92,7 @@ module.exports = function(grunt) {
 
 						'./WBC/_css/png',
 						'./WBC/_fonts',
-						'./WBC/_HTMLincludes',
+						'./WBC/_HTML/includes',
 						'./WBC/_img',
 						'./WBC/_js/libs',
 						'./WBC/_less',
