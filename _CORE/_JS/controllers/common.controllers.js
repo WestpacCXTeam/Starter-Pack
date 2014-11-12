116c5a5909df45abc6a774114f44b98c4ac84479
sean <seankerr@westpac.com.au>
2014-11-12 16:24:42 +1100

    Changes:
    Remove some stuff from the controller, remove the stuff

--- _CORE/_JS/controllers/common.controllers.js
+++ _CORE/_JS/controllers/common.controllers.js
@@ -5,7 +5,7 @@
     module.controller('MainCtrl', ['$scope', '$rootScope', '$window',
         function ($scope, $rootScope, $window) {
 
-        $scope.message = "this is a message from the $scope of the first controller";
+            //do some stuff inside the controller
 
         }
     ])
