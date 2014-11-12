6c346c6a120467603e46759f61b4ce6391510196
sean <seankerr@westpac.com.au>
2014-11-12 16:06:36 +1100

    Add basic controller functionality

---
+++ _CORE/_JS/controllers/common.controllers.js
@@ -0,0 +1,13 @@
+
+
+(function (module) {
+    //initial controller
+    module.controller('MainCtrl', ['$scope', '$rootScope', '$window',
+        function ($scope, $rootScope, $window) {
+
+           //console.log("test");
+
+        }
+    ])
+
+}(angular.module('common.controllers',[])));
